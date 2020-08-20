0cbc8bae0d3205893b51abcac0c08cbfc403eba3
Tomoya KASHIMADA <tomoya@sforzando.co.jp>
2020-08-20 12:50:41 +0900

    :pencil2: Add explanation of rps_judge_table

--- rps_env.py
+++ rps_env.py
@@ -1,5 +1,23 @@
-game_map = {0: "グー", 1: "パー", 2: "チョキ"}
-rps_table = ((-1, 1, 0), (1, -1, 2), (0, 2, -1))
+rps_map = {0: "グー", 1: "パー", 2: "チョキ"}
+
+rps_judge_table = ((-1, 1, 0), (1, -1, 2), (0, 2, -1))
+"""
+Each number mean is winner's hand form.
+'0', '1' and '2' are referenced rps_map. '-1' is no winner (equal 'Draw').
+
+e.g.
+    `rps_judge_table[0][2]` is return `0`.
+    The first selected "グー" and the second selected "チョキ".
+    Winner's hand form is "グー", and winner is the first.
+
+    `rps_judge_table[1][2]` is return `2`.
+    The first selected "パー" and the second selected "チョキ".
+    Winner's hand form is "チョキ", and winner is the second.
+
+    `rps_judge_table[1][1]` is return `-1`.
+    The first selected "パー" and the second selected "バー".
+    It is no winner and judged "Draw".
+"""
 
 player = ["源静香", "ドラえもん", "野比のび太", "骨川スネ夫", "ドラミ"]
 players_rps = {
