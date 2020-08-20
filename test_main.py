282807dd95ea36d420bdcb68688f12d1b64312f9
Tomoya KASHIMADA <tomoya@sforzando.co.jp>
2020-08-20 13:15:06 +0900

    :100: Add test

--- test_main.py
+++ test_main.py
@@ -1,6 +1,13 @@
+import pytest
+from tqdm import tqdm
+
+from itertools import product
 from random import randint
-from main import rps
+from timeit import timeit
+
+from main import main, rps
 from player import Player
+import rps_env
 
 
 def test_win_rate_of_shizuka():
@@ -14,3 +21,17 @@ def test_win_rate_of_shizuka():
         rps(shizuka, players[randint(0, len(players) - 1)], 1)
 
     assert 23.33 < shizuka.win_rate() and shizuka.win_rate() < 43.33
+
+
+@pytest.mark.skip()
+def measure_time(stmt: str, trials: int = 100) -> float:
+    t = timeit(stmt, globals=globals(), number=trials)
+    print(f"{stmt} took {t} seconds for {trials} attempts.")
+    return t
+
+
+def test_main():
+    assert dir(main)
+    matches = list(product(rps_env.player, repeat=2))
+    for first, second in tqdm(matches):
+        assert measure_time(f"main('{first}', '{second}', 1000)", 16) < 1.0
