fe2d2e36917b823d583359ba0f31c78bf14f1a5a
Tomoya KASHIMADA <tomoya@sforzando.co.jp>
2020-08-23 01:53:34 +0900

    :wrench: Rename variables and methods

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-from player import Player
 import rps_env
+from player import Player
 
 
 def main(first: str, second: str, trials: int):
@@ -36,9 +36,9 @@ def main(first: str, second: str, trials: int):
     ("グー", "パー", "Lose"), ("チョキ", "チョキ", "Draw"), ("グー", "パー", "Lose"), ("チョキ", "パー", "Lose"), ("チョキ",
     "チョキ", "Lose")] 0.00 %
     """
-    if not isinstance(first, str) or first not in rps_env.player or \
-       not isinstance(second, str) or second not in rps_env.player or \
-       not isinstance(trials, int) or trials < 0 or 10000 < trials:
+    if not isinstance(first, str) or first not in rps_env.players or \
+        not isinstance(second, str) or second not in rps_env.players or \
+        not isinstance(trials, int) or trials < 0 or 10000 < trials:
         raise ValueError()
 
     player_1st = Player(first)
@@ -46,39 +46,38 @@ def main(first: str, second: str, trials: int):
 
     results = rps(player_1st, player_2nd, trials)
 
-    win_rate = player_1st.win_rate()
+    win_rate = player_1st.get_rate_of_win()
     print(f"{results} {win_rate:.2f} %")
 
 
 def rps(player_1st: Player, player_2nd: Player, trials: int):
     results = []
     for i in range(trials):
-        player_1st_rps = player_1st.select_rps()
-        player_2nd_rps = player_2nd.select_rps()
-        winner_rps = rps_env.rps_judge_table[player_1st_rps][player_2nd_rps]
-        if winner_rps == player_1st_rps:
+        player_1st_hand = player_1st.select_hand()
+        player_2nd_hand = player_2nd.select_hand()
+        winner_rps = rps_env.rps_judge_table[player_1st_hand][player_2nd_hand]
+        if winner_rps == player_1st_hand:
             player_1st.record_result("Win")
             player_2nd.record_result("Lose")
-        elif winner_rps == player_2nd_rps:
+        elif winner_rps == player_2nd_hand:
             player_1st.record_result("Lose")
             player_2nd.record_result("Win")
         else:
             player_1st.record_result("Draw")
             player_2nd.record_result("Draw")
-        results.append((rps_env.rps_map[player_1st_rps],
-                        rps_env.rps_map[player_2nd_rps],
+        results.append((rps_env.rps_map[player_1st_hand],
+                        rps_env.rps_map[player_2nd_hand],
                         player_1st.results[-1]))
     return results
 
 
 if __name__ == "__main__":
     import argparse
+
     parser = argparse.ArgumentParser()
-    parser.add_argument("--first", type=str, default="源静香", choices=[
-                        "源静香", "ドラえもん", "野比のび太", "骨川スネ夫", "ドラミ"])
-    parser.add_argument("--second", type=str, default="ドラミ", choices=[
-                        "源静香", "ドラえもん", "野比のび太", "骨川スネ夫", "ドラミ"])
-    parser.add_argument("--trials", type=int, default=1000)
+    parser.add_argument("--first", type=str, default="源静香", choices=rps_env.players, help="Select the first player")
+    parser.add_argument("--second", type=str, default="ドラミ", choices=rps_env.players, help="Select the second player")
+    parser.add_argument("--trials", type=int, default=1000, help="Set number of trials. max:10000")
     args = parser.parse_args()
 
     main(args.first, args.second, args.trials)
