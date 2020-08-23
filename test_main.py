e1b5900d30b9fa982a4d135ff7adf7b1d8c3a264
Tomoya KASHIMADA <tomoya@sforzando.co.jp>
2020-08-23 14:52:08 +0900

    :100: Add test for Player class

--- test_main.py
+++ test_main.py
@@ -1,31 +1,11 @@
 from itertools import product
-from math import isclose
 from timeit import timeit
 
 import pytest
 from tqdm import tqdm
 
 import rps_env
-from main import main, rps
-from player import Player
-
-
-def test_rate_of_shizuka_selected(trials: int = 10000, relative_tolerance: float = 0.1):
-    shizuka = Player("源静香")
-    doraemon = Player("ドラえもん")
-    nobita = Player("野比のび太")
-    suneo = Player("骨川スネ夫")
-    dorami = Player("ドラミ")
-    players = [shizuka, doraemon, nobita, suneo, dorami]
-
-    for player in tqdm(players):
-        rps(shizuka, player, round(trials / len(players)))
-
-    print(shizuka.get_rate_of_selected_hand(0))
-
-    assert isclose(100 / 3, shizuka.get_rate_of_selected_hand(0), rel_tol=relative_tolerance)
-    assert isclose(100 / 3, shizuka.get_rate_of_selected_hand(1), rel_tol=relative_tolerance)
-    assert isclose(100 / 3, shizuka.get_rate_of_selected_hand(2), rel_tol=relative_tolerance)
+from main import main
 
 
 def test_main_arguments_failure():
